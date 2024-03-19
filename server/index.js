214336b57b4d4249eb22485b3b1ce5bea1a29461
midezen <victordiya90@gmail.com>
2024-03-19 18:02:47 +0100

    created user model

--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.mongoURL).then(() => console.log("db connected")).catch((err) => console.log("db is not connected. This is the error" + err))
+mongoose
+  .connect(process.env.mongoURL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log("db is not connected. This is the error" + err));
 
 app.listen(8000, () => {
-    console.log("app is connected and listening on port 8000")
-})
+  console.log("app is connected and listening on port 8000");
+});
