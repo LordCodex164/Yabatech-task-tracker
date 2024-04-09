064c56beaa853140df877e707e6fd2bf34b35799
LordCodex <adenirandaniel565@gmail.com>
2024-04-09 10:28:20 +0100

    added few changes

--- Frontend/src/backend/User.ts
+++ Frontend/src/backend/User.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import toast from "react-hot-toast"
 
-const BASE_URL = "http://localhost:8000/api/user"
+const BASE_URL = "https://yabatech-task-tracker.onrender.com/api/user"
 
 export const getAllUsers = async () => {
     try {
