f0f958661a556323be329130d223acc9f4a43d09
LordCodex <adenirandaniel565@gmail.com>
2024-04-09 17:51:38 +0100

    resolve token issue

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://yabatech-task-tracker-1.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 };
