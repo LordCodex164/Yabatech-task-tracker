36c582141b420a4559ae4906ce206944d57a656a
LordCodex <adenirandaniel565@gmail.com>
2024-04-11 03:55:03 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use( cors({
 app.options("*", cors(
   { credentials: true,
    methods: ["GET", "POST", "PUT", "DELETE"],
-  exposedHeaders: ["set-cookie"]
+  exposedHeaders: ["*"]
 }))
 
 mongoose
