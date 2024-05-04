1b156ec234ccb349339de2ae06f34841c5739de1
Ayomide Oluwadiya <victordiya90@gmail.com>
2024-05-04 21:06:59 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, require: true, unique: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
+idNumber: { type: String, require: true },
     isAdmin: { type: Boolean, require: true },
     tasks: [
       {
