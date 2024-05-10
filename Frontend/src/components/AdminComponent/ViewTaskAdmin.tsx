02babcfdb811a762779afb16075bcd3468db3769
LordCodex <adenirandaniel565@gmail.com>
2024-05-10 11:57:26 +0100

    adjusted styling

--- Frontend/src/components/AdminComponent/ViewTaskAdmin.tsx
+++ Frontend/src/components/AdminComponent/ViewTaskAdmin.tsx
@@ -43,16 +43,16 @@ const ViewTaskAdmin = ({staffId, specificStaff, close, open}: any) => {
         </>:
         <>
         
-        <div className='min-h-screen fixed right-0 top-0 bottom-0 shadow-transparent bg-[#6183cd] px-5 py-10 min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
+        <div className='min-h-screen fixed right-0 top-0 bottom-0 shadow-transparent bg-[#6183cd] px-5 py-10 z-50 min-w-[300px] md:min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
         <span className='cursor w-full flex justify-end cursor-pointer group' onClick={close}>
             <svg className="w-3 h-3 group-hover:text-white:" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
         </span>
-           <p className='text-[20px] font-bold'>Staff Tasks</p>
+           <p className='text-[20px] font-bold mt-[20px]'>Staff Tasks</p>
            <ul className='flex flex-col gap-[50px] mt-[20px]'>
                 {specificStaff.tasks.map((item:tasksProps) => (
-                    <li className='flex justify-between'>
+                    <li className='flex flex-row items-center justify-between'>
                       <span>{item.taskName}</span>
                       <span><button onClick={() => handleDeleteTask(item._id as number)}  className='bg-red-500 px-[30px] py-[16px]'>Delete</button></span>
                     </li>
