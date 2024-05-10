5b22a1afd45156ef7041946e3a98b50df482fd95
LordCodex <adenirandaniel565@gmail.com>
2024-05-10 12:13:51 +0100

    adjusted styling

--- Frontend/src/components/AdminComponent/AssignForm.tsx
+++ Frontend/src/components/AdminComponent/AssignForm.tsx
@@ -66,7 +66,7 @@ const AssignForm = ({close, username, email}: AssignFormProps) => {
 
   return (
     <>
-    <div className='fixed right-0 top-0 bottom-0 shadow-transparent bg-blue-300 px-5 py-10 min-w-[320px] lg:min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
+    <div className='fixed right-0 top-0 bottom-0 shadow-transparent bg-blue-300 px-5 py-10 min-w-[320px] z-40 lg:min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
        {/* let create the menu */}
        <div className='flex gap-[10px] items-center'>
 
--- Frontend/src/components/Profile/EditProfile.tsx
+++ Frontend/src/components/Profile/EditProfile.tsx
@@ -38,7 +38,7 @@ const EditProfile = ({userTasks, close}: {userTasks: userType, close: () => void
     }
 
   return (
-    <div className='fixed right-0 top-0 bottom-0 shadow-transparent bg-[#8996d7] px-5 py-10 min-w-[320px] md:min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
+    <div className='fixed right-0 top-0 bottom-0 shadow-transparent bg-[#8996d7] px-5 py-10 z-50 min-w-[320px] md:min-w-[450px] animate-slide-in-right translate-x-[6px] before:translate-x-[-66px] duration-150 transition-transform after:translate-x-[66px]'>
     {/* let create the menu */}
     <div className='flex gap-[10px] items-center'>
 
