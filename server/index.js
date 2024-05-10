51b0110a4f127cf3fc676c8af02d0f481c16a1c9
LordCodex <adenirandaniel565@gmail.com>
2024-05-10 06:18:09 +0100

    updated the server-file

--- server/index.js
+++ server/index.js
@@ -35,7 +35,6 @@ app.options(
     origin: "https://dazzling-praline-5c3ff0.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     exposedHeaders: ["*"],
-   optionsSuccessStatus: 204,
     preflightContinue: false,
     optionsSuccessStatus: 204
   })
