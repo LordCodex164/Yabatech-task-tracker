13aff7ab01733b08356dbc8af4460616608b3317
LordCodex <adenirandaniel565@gmail.com>
2024-05-10 06:20:57 +0100

    adding padding to the input component

--- Frontend/src/pages/auth/Login.tsx
+++ Frontend/src/pages/auth/Login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
           <div>
             <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} type="email"  required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              <input id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} type="email"  required className="block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
           </div>
 
@@ -30,7 +30,7 @@ const Login = () => {
               <label className="block text-sm font-medium leading-6 text-gray-900">Password</label>
             </div>
             <div className="mt-2">
-              <input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 text-blue-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              <input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 pl-3 text-blue-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
           </div>
 
